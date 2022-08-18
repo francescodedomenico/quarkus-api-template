e5213f52d0fea6fa752bf419b4e3d3e807da259f
Francesco De Domenico <dedomenico.francesco@outlook.com>
2022-08-18 14:54:41 +0200

    added swagger generationte

--- src/main/java/it/template/GreetingResource.java
+++ src/main/java/it/template/GreetingResource.java
@@ -5,10 +5,16 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import org.eclipse.microprofile.openapi.annotations.Operation;
+
 @Path("/hello")
 public class GreetingResource {
 
     @GET
+    @Operation(
+        summary = "Hello world API",
+        description = "Hello World description"
+    )
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
         return "Hello from RESTEasy Reactive, prova prova";
