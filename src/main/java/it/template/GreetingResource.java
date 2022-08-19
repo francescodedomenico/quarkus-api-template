22a1069b53556388761cf17b94a6813b238f5693
Francesco De Domenico <dedomenico.francesco@outlook.com>
2022-08-19 17:12:41 +0200

    added APIs

--- src/main/java/it/template/GreetingResource.java
+++ src/main/java/it/template/GreetingResource.java
@@ -1,6 +1,13 @@
 package it.template;
 
+import java.util.Collections;
+import java.util.LinkedHashMap;
+import java.util.Set;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
@@ -12,6 +19,14 @@ import it.template.model.ExampleModel;
 
 @Path("/")
 public class GreetingResource {
+
+    private Set<ExampleModel> examples = Collections.newSetFromMap(Collections.synchronizedMap(new LinkedHashMap<>()));
+
+    public GreetingResource() {
+        examples.add(new ExampleModel("Apple", "Winter fruit"));
+        examples.add(new ExampleModel("Pineapple", "Tropical fruit"));
+    }
+
 
     @GET
     @Operation(
@@ -40,8 +55,31 @@ public class GreetingResource {
     )
     @Path("/example_model")
     @Produces(MediaType.APPLICATION_JSON)
-    public ExampleModel getExample() {
-        ExampleModel toReturn = new ExampleModel("fieldA", "fieldB");
-        return toReturn;
+    public Set<ExampleModel> getExample() {
+        return examples;
+    }
+    @POST
+    @Operation(
+        summary = "Endpoint to test Exception handler",
+        description = "Endpoint to test Exception handle"
+    )
+    @Path("/example_model")
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Set<ExampleModel> addExample(ExampleModel exampleModel) {
+        examples.add(exampleModel);
+        return examples;
+    }
+    @DELETE
+    @Operation(
+        summary = "Endpoint to test Exception handler",
+        description = "Endpoint to test Exception handle"
+    )
+    @Path("/example_model")
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Set<ExampleModel> deleteExample(ExampleModel exampleModel) {
+        examples.remove(exampleModel);
+        return examples;
     }
 }
